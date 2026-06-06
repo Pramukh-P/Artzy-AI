dfe1d64b10295e182bfabcc2fb5947c80a1f5f03
Pramukh <pramukh.p1404@gmail.com>
2026-06-06 22:34:47 +0530

    Added project screenshots and demo

--- server/routes/promptBotRoutes.js
+++ server/routes/promptBotRoutes.js
@@ -4,7 +4,7 @@ import { checkContent } from '../utils/contentFilter.js';
 import { InferenceClient } from "@huggingface/inference";
 dotenv.config();
 const hf = new InferenceClient(
-  process.env.HF_TOKEN
+  process.env.HUGGINGFACE_API_KEY
 );
 const router = express.Router();
 
